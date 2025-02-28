8e8f63c5fdcddb41ca0a310a5521d794b67715a5
DanielEcm25 <destebancontreras@ucundinamarca.edu.co>
2025-02-27 20:44:32 -0500

    Another

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Holaa");
         System.out.println("Tipos de datos");
+        System.out.println("Ojalá esto sirva");
     }
 }
